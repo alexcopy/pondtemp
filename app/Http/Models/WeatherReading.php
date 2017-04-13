df4931b932202270f26fe085ffc55245f4d92974
Alex Redko <alexr@nr-online.com>
2017-04-13 23:02:17 +0100

    Add shed to weather

--- app/Http/Models/WeatherReading.php
+++ app/Http/Models/WeatherReading.php
@@ -20,6 +20,7 @@ class WeatherReading extends Model
         'readingDate',
         'pond',
         'street',
+        'shed',
         'shedhumid',
         'streethumid',
         'room',
@@ -35,6 +36,7 @@ class WeatherReading extends Model
         self::create([
             'readingDate' => (new DateTime())->format('Y-m-d H:i:s'),
             'pond' => (double)$request->get('ptemp', 0),
+            'shed' => (double)$request->get('shedtemp', 0),
             'street' => (double)$request->get('strtemp', 0),
             'shedhumid' => (double)$request->get('shedhumid', 0),
             'streethumid' => (double)$request->get('streethumid', 0),
