515f71c5defc6447ff69e7e296eb1e2e74e477fa
Alex Redko <alexr@nr-online.com>
2017-04-13 20:25:14 +0100

    add storage path

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -30,7 +30,7 @@ class ApiController extends Controller
 
         $fileContent = $date->format('Y-m-d H:i:s') . "  pond temp: " . $ptemp . " shedTemp: " . $shedtemp . " streetTemp:" . $strtemp . " shedhumid:" . $shedhumid . " streethumid: " . $streethumid . "\n";
 
-        $fileStat = file_put_contents("temp.txt", $fileContent, FILE_APPEND);
+        $fileStat = file_put_contents( storage_path()."/temp.txt", $fileContent, FILE_APPEND);
 
         if ($fileStat) {
 
