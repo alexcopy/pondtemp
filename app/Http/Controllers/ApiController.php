07af9c8c49db2dbd457e21f74499c74d87b0df01
Alex Redko <alexr@nr-online.com>
2017-04-14 10:50:43 +0100

    increase view records to show decrease records to write in db

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -22,17 +22,26 @@ class ApiController extends Controller
 
     public function index()
     {
-        $weather= WeatherReading::orderBy('id', 'desc')->limit(50)->get();
-        $tempReader=TempMeter::orderBy('id', 'desc')->limit(50)->get();
+        $weather = WeatherReading::orderBy('id', 'desc')->limit(300)->get();
+        $tempReader = TempMeter::orderBy('id', 'desc')->limit(300)->get();
         return view('pages.index', compact(['weather', 'tempReader']));
-     }
+    }
 
     public function tempdata(Request $request)
     {
-        $insertEvery=1200; //seconds
-        TempMeter::writeToDb((double)$request->get('ptemp', 0));
+        $insertWeatherEvery = 1200; //seconds
+        $insertTemprEvery = 3600; //seconds
+
         TempMeter::writeToTextFile($request);
-        WeatherReading::readDataAndWrite($request);
+        $weather = WeatherReading::orderBy('id', 'desc')->limit(1)->get()->first();
+        $tempReader = TempMeter::orderBy('id', 'desc')->limit(1)->get()->first();
+
+        if ($weather && $weather->timestamp > $insertWeatherEvery) {
+            WeatherReading::readDataAndWrite($request);
+        }
+        if ($tempReader && $tempReader->timestamp > $insertTemprEvery) {
+            TempMeter::writeToDb((double)$request->get('ptemp', 0));
+        }
 
     }
 }
