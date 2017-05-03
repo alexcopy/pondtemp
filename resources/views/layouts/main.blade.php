23381660f1617fceddcdf11490ee10f04bcdb3b1
Alex Redko <alexr@nr-online.com>
2017-05-04 00:05:07 +0100

    change security policy

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,8 +2,6 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' https://* http://themes.googleusercontent.com/* 'unsafe-inline'; script-src 'self' https://* http://themes.googleusercontent.com/* 'unsafe-inline' 'unsafe-eval'"/>
-
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Latest compiled and minified CSS -->
