69c42af15b555dd7f7681b4016622877561a302a
Alex Redko <alexr@nr-online.com>
2017-07-01 01:01:47 +0100

    change layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,24 +8,88 @@
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="/assets/css/bootstrap.css">
     <link rel="stylesheet" href="/assets/css/awesome.css">
-  @yield('custom_css')
-      <title>PondTemp</title>
+
+    @yield('custom_css')
+    <title>PondTemp</title>
+
+
+    @yield('custom_css')
+    <title>{{ config('app.name', 'PondTemp') }}</title>
 
 </head>
 <body>
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
 
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-<div class="container-fluid" role="main">
-    @yield('tabs')
-    <br/>
-    <div class="panel">
-        @yield('subtabs')
-        <div class="panel-heading">
-            <h4 class="panel-title">@yield('panelHeader') </h4>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'PondTemp') }}
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        {{--<li><a href="{{ route('register') }}">Register</a></li>--}}
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                          style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
         </div>
-        <div class="panel-body">
-            <br>
-            @yield('content')
+    </nav>
+
+    <div class="container-fluid" role="main">
+        @yield('tabs')
+        <br/>
+        <div class="panel">
+            @yield('subtabs')
+            <div class="panel-heading">
+                <h4 class="panel-title">@yield('panelHeader') </h4>
+            </div>
+            <div class="panel-body">
+                <br>
+                @yield('content')
+            </div>
         </div>
     </div>
 </div>
