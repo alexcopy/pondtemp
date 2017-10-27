68b0359424292b6ebd3279ba7e4dee617728b3d5
Alex Redko <alexcopy@mail.ru>
2017-10-27 15:09:44 +0100

    Fix Log Viewer Home link

--- resources/views/vendor/log-viewer/_template/master.blade.php
+++ resources/views/vendor/log-viewer/_template/master.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LogViewer</title>
     <meta name="description" content="LogViewer">
-    <meta name="author" content="ARCANEDEV">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.15.35/css/bootstrap-datetimepicker.min.css">
--- resources/views/vendor/log-viewer/_template/navigation.blade.php
+++ resources/views/vendor/log-viewer/_template/navigation.blade.php
@@ -7,8 +7,8 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a href="{{ route('log-viewer::dashboard') }}" class="navbar-brand">
-                <i class="fa fa-fw fa-book"></i> LogViewer
+            <a href="/" class="navbar-brand">
+                <i class="fa fa-fw fa-book"></i> PondTemp
             </a>
         </div>
         <div class="collapse navbar-collapse" id="navbar">
