d0a66169f0b6724c5b3173673903c041b00e2a41
Alex Red <alexcopy@mail.ru>
2018-07-13 00:54:58 +0100

    add query logging

--- app/Http/Controllers/PusherController.php
+++ app/Http/Controllers/PusherController.php
@@ -7,8 +7,21 @@ use Illuminate\Http\Request;
 class PusherController extends Controller
 {
 
-    public function webhooks(Request $request)
+    public function webhooks(Request $req)
     {
+        $req->input('events');
+
+        file_put_contents(storage_path() . "/smslog.txt", $req->getQueryString() . "\n", FILE_APPEND);
+//        foreach ($request()->input('events') as $event) {
+//            $channel = $event[‘channel’];
+//            $user_id = $event[‘user_id’];
+//            $name = $event[‘name’];
+
+          //  $event = Pusher::event($channel, $user_id, $name);
+
+           //if ($event->isValid()) $event->handle();
+//        }
+
         return response([], 200);
     }
 
