342fe1538153996804f69e956670f6e313ee619c
Alex Red <alexcopy@mail.ru>
2019-07-04 10:35:15 +0100

    Add device types

---
+++ database/migrations/2019_07_04_092631_create_device_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeviceTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('device_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pond_id');
+            $table->string('deviceType');
+            $table->string('description');
+            $table->tinyInteger('is_disabled')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('device_types');
+    }
+}
